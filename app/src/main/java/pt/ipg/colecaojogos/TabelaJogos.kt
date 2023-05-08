c259d5175f38fee5c0873bc44d32d9f7f42cbf66
Andre Antunes  1706033 <aavalentim2@hotmail.com>
2023-05-08 21:24:57 +0100

    Criado o TabelaJogos.kt, neste foi criada uma função e um companion object

---
+++ app/src/main/java/pt/ipg/colecaojogos/TabelaJogos.kt
@@ -0,0 +1,14 @@
+package pt.ipg.colecaojogos
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaJogos(db: SQLiteDatabase) : TabelaBD(db, NOME_TABELA) {
+    override fun cria() {
+        
+    }
+
+    companion object {
+        const val NOME_TABELA = "jogos"
+    }
+}
