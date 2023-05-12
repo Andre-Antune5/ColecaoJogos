d526a86590465d5632c030eb712b5efbb15955fd
Andre Antunes  1706033 <aavalentim2@hotmail.com>
2023-05-12 09:18:20 +0100

    Criada função para converter os valores em ContentValues para a tabela Jogo

--- app/src/main/java/pt/ipg/colecaojogos/Jogo.kt
+++ app/src/main/java/pt/ipg/colecaojogos/Jogo.kt
@@ -1,5 +1,17 @@
 package pt.ipg.colecaojogos
 
-class Jogo (var nome: String, var id: Long = -1, var desenvolvedor: String, var data: String, var preco: Double, var id_categoria: Int) {
+import android.content.ContentValues
 
+class Jogo (var nome: String, var id: Long = -1, var desenvolvedor: String, var data: String, var preco: Double, var id_categoria: Int) {
+    fun toContentValues(): ContentValues {
+        val valores = ContentValues()
+
+        valores.put(TabelaJogos.CAMPO_NOME, nome)
+        valores.put(TabelaJogos.CAMPO_DESENVOLVEDOR, desenvolvedor)
+        valores.put(TabelaJogos.CAMPO_DATA, data)
+        valores.put(TabelaJogos.CAMPO_PRECO, preco)
+        valores.put(TabelaJogos.CAMPO_FK_CATEGORIA, id_categoria)
+
+        return valores
+    }
 }
--- app/src/main/java/pt/ipg/colecaojogos/TabelaJogos.kt
+++ app/src/main/java/pt/ipg/colecaojogos/TabelaJogos.kt
@@ -5,10 +5,15 @@ import android.provider.BaseColumns
 
 class TabelaJogos(db: SQLiteDatabase) : TabelaBD(db, NOME_TABELA) {
     override fun cria() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, nome TEXT NOT NULL, desenvolvedor TEXT, data TEXT, preco REAL, id_categoria INTEGER NOT NULL, FOREIGN KEY (id_categoria) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, $CAMPO_NOME TEXT NOT NULL, $CAMPO_DESENVOLVEDOR TEXT, $CAMPO_DATA TEXT, $CAMPO_PRECO REAL, $CAMPO_FK_CATEGORIA INTEGER NOT NULL, FOREIGN KEY ($CAMPO_FK_CATEGORIA) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
     }
 
     companion object {
         const val NOME_TABELA = "jogos"
+        const val CAMPO_NOME = "nome"
+        const val CAMPO_DESENVOLVEDOR = "desenvolvedor"
+        const val CAMPO_DATA = "data"
+        const val CAMPO_PRECO = "preco"
+        const val CAMPO_FK_CATEGORIA = "id_categoria"
     }
 }
