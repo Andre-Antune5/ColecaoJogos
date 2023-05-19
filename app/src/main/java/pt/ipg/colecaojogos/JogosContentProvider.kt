d094b4b56990a7ed51dfc04c33f07201c9e1186c
Andre Antunes  1706033 <aavalentim2@hotmail.com>
2023-05-19 10:16:34 +0100

    Adicionado companion Object ao JogosContentProvider.kt

--- app/src/main/java/pt/ipg/colecaojogos/JogosContentProvider.kt
+++ app/src/main/java/pt/ipg/colecaojogos/JogosContentProvider.kt
@@ -2,6 +2,7 @@ package pt.ipg.colecaojogos
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 
@@ -11,7 +12,7 @@ class JogosContentProvider : ContentProvider() {
     }
 
     override fun query(
-        p0: Uri,
+        p0: Uri, //URI = universal Resource Identifier
         p1: Array<out String>?,
         p2: String?,
         p3: Array<out String>?,
@@ -36,4 +37,18 @@ class JogosContentProvider : ContentProvider() {
         TODO("Not yet implemented")
     }
 
+
+    companion object {
+        private const val AUTORIDADE = "pt.ipg.colecaojogos"
+        private const val URI_CATEGORIAS = 100
+        private const val URI_JOGOS = 200
+        const val CATEGORIAS = "categorias"
+        const val JOGOS = "jogos"
+
+
+        fun uriMatcher() = UriMatcher(UriMatcher.NO_MATCH).apply {
+            addURI(AUTORIDADE, CATEGORIAS, URI_CATEGORIAS) // content://pt.ipg.colecaojogos/categorias
+            addURI(AUTORIDADE, JOGOS, URI_JOGOS) //content://pt.ipg.colecaojogos/jogo
+        }
+    }
 }
