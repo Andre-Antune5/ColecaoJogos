29845a7a346c44129ee6e37a528a3a7375e60dff
Andre Antunes  1706033 <aavalentim2@hotmail.com>
2023-05-26 09:53:30 +0100

    Inicializar o RecyclerView de Jogos (adapter e layoutManager)

--- app/src/main/java/pt/ipg/colecaojogos/ListaJogos_Fragment.kt
+++ app/src/main/java/pt/ipg/colecaojogos/ListaJogos_Fragment.kt
@@ -34,7 +34,7 @@ class ListaJogos_Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lista_jogos_, container, false)
+        return inflater.inflate(R.layout.fragment_lista_jogos, container, false)
     }
 
     companion object {
